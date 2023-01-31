53529635b011d76921f24bf7814cf0e8d2c5301b
Morella92 <morena.piem@outlook.com>
2023-01-31 17:43:17 +0100

    FEAT description

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-// 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-//
-// 
-// 
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, 
-// per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 // il numero di chilometri che vuole percorrere
 const kmNumber = parseInt( prompt ('Quanti kilometri vuoi percorrere?') ) 
-console.log(kmNumber)
+console.log('Numero kilometri da percorrere:' + kmNumber)
 
 // età del passeggero
 const agePassenger = parseInt ( prompt('Quanti anni hai?') )
-console.log(agePassenger)
+console.log('Età passeggero:' + agePassenger)
 
 // 1-il prezzo del biglietto è definito in base ai km (0.21 € al km)
 const ticketPrice = parseFloat ( 0.21 * kmNumber)
-console.log(ticketPrice)
+console.log('Prezzo del biglietto in base alla tratta:' + ticketPrice)
 
 let discount = 0
 
@@ -28,12 +22,12 @@ if (agePassenger <= 18) {
     discount = 0.2;
     let discountPrice = (ticketPrice - (ticketPrice * discount)) 
     let roundedPrice = parseFloat(discountPrice.toFixed(2))
-    console.log(roundedPrice)
+    console.log('Il prezzo scontato del 20% è:' + roundedPrice)
 } else if (agePassenger >= 65) {
     discount = 0.4;
     let discountPrice = (ticketPrice - (ticketPrice * discount))
     let roundedPrice = parseFloat(discountPrice.toFixed(2))
-    console.log(roundedPrice)
+    console.log('Il prezzo scontato del 20% è:' + roundedPrice)
 }
 
 
